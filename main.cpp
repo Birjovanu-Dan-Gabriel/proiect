14922e267d1698b0b700771d73e81b2ac2932c71
Birjovanu_Dan <birjvoanudan123@gmail.com>
2024-11-29 19:06:03 +0200

    Test 1

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
-#include "Game.h"
-#
 
 int main()
 {
 	//init rand
-	std::srand(static_cast<unsigned>(time(NULL)));
-
-
-	//initializare game
-	Game game;
-	game.run();
+	std::cout<< "a";
 	return 0;
 }
